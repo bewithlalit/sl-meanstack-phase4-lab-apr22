4e7fb84d56971a9df04175489fad5d98c9f3e821
bewithlalit <bewithlalit@gmail.com>
2022-04-23 21:22:16 +0530

    Add files via upload

---
+++ Nodejs app using docker/simple.js
@@ -0,0 +1,10 @@
+console.log("Welcome simple nodejs program using docker");
+let a=10;
+let b=20;
+let sum = a + b;
+console.log("Sum of two number is "+sum);
+function sayHello(name) {
+    return "welcome "+name;
+}
+console.log(sayHello("Lalit"));
+
